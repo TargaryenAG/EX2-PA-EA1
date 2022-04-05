f9421730e3f28504e9e9fd58e14dab9f19e0f81f
NathanAG <82623281+TargaryenAG@users.noreply.github.com>
2022-04-05 16:51:40 +0000

    switch case para preencher a matriz com os dados do teclado

--- main.c
+++ main.c
@@ -12,7 +12,52 @@ int main(){
   scanf ("%s",palavra);
   tam = strlen(palavra);
 
+for(int L=0;L<8;L++){
+  switch(L){
 
+    case 0:
+      for(int C=0;C<3;C++){
+        if(C==0){
+          matrix[L][C]='a';
+        }else{
+          matrix[L][C]=matrix[L][C-1]+1;
+        }
+      }
+    break;
+
+    case 5:
+    case 7:
+      for(int C=0;C<4;C++){
+        if(C==0){
+          matrix[L][C]=matrix[L-1][C+2]+1;
+        }else{
+          matrix[L][C]=matrix[L][C-1]+1;
+        }
+      }
+    break;
+
+    case 6:
+      for(int C=0;C<3;C++){
+        if(C==0){
+          matrix[L][C]=matrix[L-1][C+3]+1;
+        }else{
+          matrix[L][C]=matrix[L][C-1]+1;
+        }
+      }
+    break;
+
+    default:
+      for(int C=0;C<3;C++){
+        if(C==0){
+          matrix[L][C]=matrix[L-1][C+2]+1;
+        }else{
+          matrix[L][C]=matrix[L][C-1]+1;
+        }
+      }
+    break;
+  }
+}
+  printf("\n\n");
   
   return 0;
 }
