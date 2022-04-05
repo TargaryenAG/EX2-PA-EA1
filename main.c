1bf7d94f2f572ff96d6bd626bff261b17ac677b8
NathanAG <82623281+TargaryenAG@users.noreply.github.com>
2022-04-05 16:13:04 +0000

    Declarando as Variáveis

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+#include < stdlib.h > 
+#include < stdio.h >
+int main(){
+  
+  int Tam, colum;
+  string DigUsuário;
+  Char matrix[8[4];
 
-int main(void) {
-  printf("Hello World\n");
+ 
+  
+
   return 0;
 }
